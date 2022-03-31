35759f690fb7a312aa6b2b4c79d06b6e8e1e877e
calvin <ticlacalvin@yahoo.com>
2022-03-31 18:12:01 -0400

    code for tonight

---
+++ college.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+bucket_name=('dev2022')
+
+#for bucketname in "$bucket_name"
+for bucketname in ${bucket_name[*]}
+
+do
+	echo "check every name in new line if already exists"
+	aws s3api wait bucket-exists --bucket $bucket_name
+
+#in case that name is not owned then;
+
+	echo "creating a bucket with a name from the list"
+       	aws s3 mb s3://"$bucket_name"
+done
+
+
+
+
+
+
+
+
+
+
+		
+		
